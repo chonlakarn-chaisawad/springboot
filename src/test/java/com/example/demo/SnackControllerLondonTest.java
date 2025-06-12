c9f2e467e0ccdd564188dfe268860b093ad1246d
view <viuu@odds.team>
2025-06-12 09:03:55 +0700

    fix test happy case in london style

--- src/test/java/com/example/demo/SnackControllerLondonTest.java
+++ src/test/java/com/example/demo/SnackControllerLondonTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.apache.coyote.BadRequestException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,8 +9,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -42,6 +43,33 @@ class SnackControllerLondonTest {
         SnackModel res = snackService.createData(req);
         Assertions.assertThat(res).isNotNull();
         Assertions.assertThat(res.getId()).isNotNull();
+        verify(snackModelRepository,times(1)).save(any(SnackModel.class));
+    }
+
+    @Test
+    void updateSnack() throws BadRequestException {
+        //input
+        SnackReq req = new SnackReq();
+        SnackModel snackModel = new SnackModel();
+        SnackModel snackUpdate = new SnackModel();
+        snackModel.setId(1L);
+        snackModel.setName("candy");
+        snackModel.setPrice(10);
+        snackModel.setAmount(20);
+        req.setName("apple");
+        snackUpdate.setId(1L);
+        snackUpdate.setName("apple");
+        snackUpdate.setPrice(10);
+        snackUpdate.setAmount(20);
+
+        //Mocking
+        when(snackModelRepository.findById(1L)).thenReturn(Optional.of(snackModel));
+        when(snackModelRepository.save(any(SnackModel.class))).thenReturn(snackUpdate);
+        //assertion
+        SnackModel res = snackService.updateData(1L,req);
+        Assertions.assertThat(res).isNotNull();
+        Assertions.assertThat(res.getId()).isNotNull().isEqualTo(1L);
+        Assertions.assertThat(res.getName()).isNotNull().isEqualTo("apple");
         verify(snackModelRepository,times(1)).save(any(SnackModel.class));
     }
 }
